f53417b8477fda9ad1ac6393c49ea104ac4ac501
Patrick Childers <patrick@zimmdot.com>
2013-09-16 17:45:35 -0400

    CESDKLicenseCreator

---
+++ License/CESDKLicense/CESDKLicense.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Security.Cryptography;
+using System.Security.Cryptography.Xml;
+
+namespace CESDKLicense
+{
+    public class CESDKLicense
+    {
+        public static void AppendLicenseData(XmlDocument xmldoc)
+        {
+            // Get the key pair from the key store.
+            CspParameters parms = new CspParameters(1);
+            parms.Flags = CspProviderFlags.UseMachineKeyStore;
+            parms.KeyContainerName = "CommunityExpressSDK";
+            parms.KeyNumber = 2;
+            RSACryptoServiceProvider csp = new RSACryptoServiceProvider(parms);
+
+            // Creating the XML signing object.
+            SignedXml sxml = new SignedXml(xmldoc);
+            sxml.SigningKey = csp;
+
+            // Set the canonicalization method for the document.
+            sxml.SignedInfo.CanonicalizationMethod =
+              SignedXml.XmlDsigCanonicalizationUrl; // No comments.
+
+
+            // Create an empty reference (not enveloped) for the XPath
+            // transformation.
+            Reference r = new Reference("");
+
+            // Create the XPath transform and add it to the reference list.
+            r.AddTransform(new XmlDsigEnvelopedSignatureTransform(false));
+
+            // Add the reference to the SignedXml object.
+            sxml.AddReference(r);
+
+            // Compute the signature.
+            sxml.ComputeSignature();
+
+            // Get the signature XML and add it to the document element.
+            XmlElement sig = sxml.GetXml();
+            xmldoc.DocumentElement.AppendChild(sig);
+        }
+    }
+}
---
+++ License/CESDKLicenseCreator/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Xml;
+using System.Diagnostics;
+using System.IO;
+
+namespace CESDKLicenseCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string strAppId = textBox1.Text;
+            int appId = 0;
+
+            if (string.IsNullOrWhiteSpace(strAppId))
+            {
+                MessageBox.Show("App Id must be entered");
+                textBox1.Focus();
+                return;
+            }
+            else if (!int.TryParse(strAppId, out appId))
+            {
+                MessageBox.Show("App Id must contain only digits");
+                textBox1.Focus();
+                return;
+            }
+
+            XmlDocument document = new XmlDocument();
+
+            XmlElement licenseElement = document.CreateElement("License");
+            XmlElement AppIdElement = document.CreateElement("AppId");
+            AppIdElement.InnerText = strAppId;
+            licenseElement.AppendChild(AppIdElement);
+            document.AppendChild(licenseElement);
+
+            // license the document
+            CESDKLicense.CESDKLicense.AppendLicenseData(document);
+
+            // get the desktop directory
+            // make a subdirectory called \Licenses
+            // make a subdirectory called \Licenses\[appid]
+            // write the file to \Licenses\[appid]\CESDKLicense.xml
+
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string subPath = Path.Combine(path, @"Licenses");
+            string subsubPath = Path.Combine(subPath, strAppId);
+            string finalPath = Path.Combine(subsubPath, @"CESDKLicense.xml");
+
+            Directory.CreateDirectory(subsubPath);
+
+            using (XmlTextWriter writer = new XmlTextWriter(finalPath, Encoding.UTF8))
+            {
+                writer.Formatting = Formatting.Indented;
+                document.WriteTo(writer);
+            }
+
+            // open the folder \Licenses\[appid]\
+            ProcessStartInfo psi = new ProcessStartInfo(subsubPath);
+            psi.UseShellExecute = true;
+            Process.Start(psi);
+        }
+    }
+}
